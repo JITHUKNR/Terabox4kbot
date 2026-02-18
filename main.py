7ec5bf49a7d7b030b956f90941c09ab18b1ae368
JITHUKNR <96571770+JITHUKNR@users.noreply.github.com>
2026-02-18 17:45:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from pyrogram import Client, filters
 import requests
 
 # നിങ്ങളുടെ വിവരങ്ങൾ ഇവിടെ നൽകുക
-api_id = 1234567  # my.telegram.org-ൽ നിന്നുള്ള നമ്പർ
-api_hash = "your_api_hash" 
-bot_token = "your_bot_token"
-cookies = "your_ndus_cookie_here"
+api_id = 34818480  # my.telegram.org-ൽ നിന്നുള്ള നമ്പർ
+api_hash = "a82911ecbf23a7ead187d410a34cf47a" 
+bot_token = "8474423811:AAEWPnVwWmKpMxEROHcAy3JaMtMp4BXlwI4"
+cookies = "877a11e9d5836de7ca9e8282e0e2245cc9687996622cddcd5381d63c275180414313157ee12a92e236a7e64ec35c3fea1a4b9ee61731170effd4c70134b66b16e9644fd35824c6378fbaf36a7c15d10781698daf3ff7f29f08f426650d8899dc3f2f3a3c8edd0a2fe8f9c7dcf1a6583a"
 
 app = Client("my_terabox_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
